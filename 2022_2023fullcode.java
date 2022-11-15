422f7abb40e4c35d5c5ce69ad4d7b0106aa8f7c2
ImNotAnAmateurKV <101678251+ImNotAnAmateurKV@users.noreply.github.com>
2022-11-15 15:46:31 -0800

    strafe

--- 2022_2023fullcode.java
+++ 2022_2023fullcode.java
@@ -87,6 +87,7 @@ public class mechanumdrive extends LinearOpMode {
     //--//
     
     //--Set up the arm motors--//
+    /*
     arm_EXT.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     arm_ROT.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     arm_EXT.setTargetPosition(Help.degreesToTick(0));
@@ -163,17 +164,17 @@ public class mechanumdrive extends LinearOpMode {
         }
         else {
           if (drv_stick_x > 0) {
-            whl_RF_percent = gamepad1.right_trigger * 1;
-            whl_RB_percent = gamepad1.right_trigger * -1;
-            whl_LF_percent = gamepad1.right_trigger * -1;
-            whl_LB_percent = gamepad1.right_trigger * 1;
+            whl_RF_percent = drv_stick_x * 1;
+            whl_RB_percent = drv_stick_x * -1;
+            whl_LF_percent = drv_stick_x * -1;
+            whl_LB_percent = drv_stick_x * 1;
           }
           
           if (drv_stick_x < 0) {
-            whl_LF_percent = gamepad1.left_trigger * 1;
-            whl_LB_percent = gamepad1.left_trigger * -1;
-            whl_RB_percent = gamepad1.left_trigger * 1;
-            whl_RF_percent = gamepad1.left_trigger * -1;
+            whl_LF_percent = drv_stick_x * 1;
+            whl_LB_percent = drv_stick_x * -1;
+            whl_RB_percent = drv_stick_x * 1;
+            whl_RF_percent = drv_stick_x * -1;
           }
         }
         whl_corrections(); // Corrects/Adjusts power for correct results
@@ -290,7 +291,7 @@ public class mechanumdrive extends LinearOpMode {
       whl_RF_percent = (float) (whl_RF_percent * 0.45);
       whl_RB_percent = (float) (whl_RB_percent * 0.45 );
       whl_LF_percent = (float) (whl_LF_percent * 0.45);
-      whl_LB_percent = (float) (whl_LB_percent * 0.61);
+      whl_LB_percent = (float) (whl_LB_percent * 0.45);
   }
   
   
