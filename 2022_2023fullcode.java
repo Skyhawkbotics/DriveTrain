8d76109855dc905d18c02ba11722123fbc40773b
ImNotAnAmateurKV <101678251+ImNotAnAmateurKV@users.noreply.github.com>
2023-01-10 16:03:31 -0800

    Update 2022_2023fullcode.java

--- 2022_2023fullcode.java
+++ 2022_2023fullcode.java
@@ -1,398 +1,146 @@
+# Welcome to the OnBotJava Code Editor
 
-/*
-Packages and Imports used for the code.
-*/
-package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.TouchSensor;
+If you are just starting out, click the '+' (Add File) icon in the top left corner.
+Enter your new file name, and then choose one of the many samples.
+If you just want to drive a basic robot, select the "BasicOpMode_Linear" sample.
+Select the "TeleOp" radio button, and then click "OK".
 
-import java.lang.Math;
+The sample you chose will be renamed to match the name you entered, and it 
+will appear on the "project files" list in the left pane.
 
-/*
-Code Starts Here.
-*/
+To edit your code, just click on the desired file in the left hand pane, 
+and it will be loaded into this Code Editor window. Make any changes. You can also
+use Ctrl-F (or Cmd-F on Macs) to search the file you are working on.
 
-/*
-Hello future programmer. My name is Kevin Vu and initially wrote this code.
-It is not very readable and some of the design decisions I made are very questionable.
-| For the movement of the claw, look at the gamepadInputHandling() claw section and the clawMove() section.
-| now_time-last_time is used to find time inbetween loops and normalize degrees per loop to degrees per second.
-*/
+Once you are done, click the "Build Everything" icon at the bottom of this pane.
+This will build your OpModes and report any errors.
+If there are no errors, the OpModes will be stored on the Robot for immediate use.
 
-@TeleOp(name = "2022-2023fullcode")
-public class mechanumdrive extends LinearOpMode {
-  //Clock
-  private ElapsedTime     runtime = new ElapsedTime();
+## Samples
 
-  //Create Motor Variables
-  private DcMotor whl_LB;
-  private DcMotor whl_LF;
-  private DcMotor whl_RB;
-  private DcMotor whl_RF;
-  private DcMotorEx arm_EXT;
-  private DcMotorEx arm_ELEVATOR1;
-  private DcMotorEx arm_ELEVATOR2;
-  private DcMotorEx arm_ROT;
-  private Servo claw_GRIP;
-  private Servo wrist_ROT;
-  private Servo susan_ROT;
-  private DistanceSensor elevator_DISTSENSOR;
-  
-  //time at which the claw rotates for per movement. Modified when restarting the robot.
-  //double CLAW_ROTATE_TIME = 0.18;
-  
-  //double everything_universalscale = 1;  //Multiplier for angle/power
-  //double wheel_universalscale = 0.8; //Multiplier for power for wheels
-  //double wheel_equalizerscale = 0; //How much the difference between two sides of the wheel should be evened by (0-1)
-  float whl_LB_percent;
-  float whl_LF_percent;
-  float whl_RB_percent;
-  float whl_RF_percent;
-  float arm_EXT_angle = 0; // 0 to 1300 | retracted to fully extended
-  float arm_ROT_angle = 0;
-  float arm_ELEVATOR_angle = 0; // 0 to 3500 | lowered to fully raised
-  double susan_ROT_percent = 0.5;
-  double claw_GRIP_angle = 0.28; // 0.28 to 0.85 | closed to fully opened
-  double wrist_ROT_percent = 0.5; // >0.5 to <0.5 | move up or move down
-  
-  double susan_ROT_pos = 0;
-  double wrist_ROT_pos = 0; // Increases or decreases based on how much movement the claw makes | Utilized for finding how much to readjust the claw by to reset it.
-  
-  double last_time = runtime.seconds(); //Used to find how much time has elapsed per iteration in the runtime loop.
-  double reset_last_time = runtime.seconds(); //Last time the robot has reset
-  
-  boolean stopreset_soon = false; //Is the robot trying to reset all the motors? (Except wheels)
-  boolean arm_Sensor = false;
-  
-  boolean elevator_HasReset = false;
+There are a range of different samples to choose from.
+Sample names use a convention which helps to indicate their general, and specific, purpose.
 
-  //private DistanceSensor distance;
-  /**
-   * This function is executed when this Op Mode is selected from the Driver Station.
-   */
-  @Override
-  public void runOpMode() {
-    //Initalize Motors and Servos
-    whl_LB = hardwareMap.get(DcMotor.class, "left/back");
-    whl_LF = hardwareMap.get(DcMotor.class, "left/front");
-    whl_RB = hardwareMap.get(DcMotor.class, "right/back");
-    whl_RF = hardwareMap.get(DcMotor.class, "right/front");
-    
-    susan_ROT = hardwareMap.get(Servo.class, "susan_ROT"); // Control servo port 0
-    arm_EXT = hardwareMap.get(DcMotorEx.class, "arm_EXT"); //Expansion port 1
-    arm_ROT = hardwareMap.get(DcMotorEx.class, "arm_ROT");
-    arm_ELEVATOR1 = hardwareMap.get(DcMotorEx.class, "Elevator1");
-    arm_ELEVATOR2 = hardwareMap.get(DcMotorEx.class, "Elevator2");
+eg: The name's prefix describes the general purpose, which can be one of the following:
 
-    claw_GRIP = hardwareMap.get(Servo.class, "claw_GRIP"); //control servo port 2
-    wrist_ROT = hardwareMap.get(Servo.class, "wrist_ROT"); //control servo port 1
-    
-    //Initalize Sensors
-    elevator_DISTSENSOR = hardwareMap.get(DistanceSensor.class, "elevatorDistance");
-    
-    //--These wheels are reversed for desired results--//
-    whl_LB.setDirection(DcMotorSimple.Direction.REVERSE);
-    whl_LF.setDirection(DcMotorSimple.Direction.REVERSE);
-    //--//
-    
-    arm_ROT.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    arm_EXT.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    arm_ELEVATOR1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    arm_ELEVATOR2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+* Basic:    This is a minimally functional OpMode used to illustrate the skeleton\/structure
+            of a particular style of OpMode.  These are bare bones examples.
+* Sensor:   This is a Sample OpMode that shows how to use a specific sensor.
+            It is not intended as a functioning robot, it is simply showing the minimal code
+            required to read and display the sensor values.
+* Pushbot:  This is a Sample OpMode that uses the Pushbot robot structure as a base.
+* Concept:	This is a sample OpMode that illustrates performing a specific function or concept.
+            These may be complex, but their operation will be explained clearly in the comments,
+            or the header should reference an external doc., guide or tutorial.
+* Hardware: This is not an actual OpMode, but a helper class that is used to describe
+            one particular robot's hardware devices. eg: A Pushbot.  Look at any
+            Pushbot sample to see how this can be used in an OpMode.
+            If you add a Hardware sample to your project, you MUST use the identical name.
 
-    arm_ROT.setTargetPosition(Help.degreesToTick(0));
-    arm_EXT.setTargetPosition(Help.degreesToTick(0));
-    arm_ELEVATOR1.setTargetPosition(Help.degreesToTick(0));
-    arm_ELEVATOR2.setTargetPosition(Help.degreesToTick(0));
+For more help, visit the FTC Control System Wiki (https://github.com/FIRST-Tech-Challenge/FtcRobotController/wiki)
 
-    arm_ROT.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-    arm_EXT.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-    arm_ELEVATOR1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-    arm_ELEVATOR2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+Please report any encountered issues on [GitHub](https://github.com/FIRST-Tech-Challenge/FtcRobotController).
 
-    arm_ROT.setVelocity(20);
-    arm_EXT.setVelocity(750);
-    arm_ELEVATOR1.setVelocity(1200);
-    arm_ELEVATOR2.setVelocity(1200);
-    
-    //setup touch sensor
-    //digitalTouch.setMode(DigitalChannel.Mode.INPUT);
+## What's Java 8?
 
+Java 8 provides more ways to help you write more concise, readable, and maintainable code
+for your robot.
 
-    //--//
-    
-    
-    waitForStart();
-    
-    if (opModeIsActive()) {
-      // Start the loop
-      while (opModeIsActive()) {
-        //now_time, the time since the start of the program and is used to find time differentials between loop iterations
-        double now_time = runtime.seconds();
-        
-        // Initialization Process //
-        //5.2
-        if (!elevator_HasReset && elevator_DISTSENSOR.getDistance(DistanceUnit.CM) > 5.2) {
-          arm_ELEVATOR_angle -= 200 * (now_time-last_time);
-        }
-        else if (!elevator_HasReset && elevator_DISTSENSOR.getDistance(DistanceUnit.CM) < 5.2) {
-          elevator_HasReset = true;
-          arm_ELEVATOR1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-          arm_ELEVATOR2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-          arm_ELEVATOR_angle = 0;
-          arm_ELEVATOR1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-          arm_ELEVATOR2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        }
-        // Finished Robot intitalzieaiton//
-        ////----INPUTS----////
-        if (elevator_HasReset) {
-          gamepadInputHandling(now_time);
-        }
-        
-        
-        //--All things related to resetting the motors--// DEPRECATED FOR NOW
-        /*
-        if (now_time - reset_last_time > 4 && stopreset_soon) {
-          stopreset_soon = false;
-        }
-        else if (now_time - reset_last_time < 4 && stopreset_soon){
-          arm_extender_desiredangle-=500 * (now_time-last_time);
-          arm_rotate_desiredangle-=300 * (now_time-last_time) * everything_universalscale;
-        }
-        
-        if (gamepad1.start && !stopreset_soon) {
-          stopreset_soon = true;
-          
-          claw_grip_desiredangle = 0.28;
-          clawMove(1,claw_rotate_position, now_time);
-          claw_rotate_position = 0;
-          
-          reset_last_time = runtime.seconds();
-        }
-        */
-        last_time = now_time; //To find time differentials between loops.
-        
-        ////----VARIABLE MONITORING----////
-        
-        telemetry.addData("righttrigger", gamepad1.right_trigger);
-        telemetry.addData("lefttrigger", gamepad1.left_trigger);
-        telemetry.addData("leftstickx", gamepad1.left_stick_x);
-        telemetry.addData("leftsticky", gamepad1.left_stick_y);
-        telemetry.addData("rightstickx", gamepad1.right_stick_x);
-        telemetry.addData("rightsticky", gamepad1.right_stick_y);
-        telemetry.addData("arm_ext_desiredangle", arm_EXT_angle);
-        telemetry.addData("arm_ELEVATOR_angle", arm_ELEVATOR_angle);
-        telemetry.addData("arm_ROT_angle", arm_ROT_angle);
-        telemetry.addData("elevatordist", elevator_DISTSENSOR.getDistance(DistanceUnit.CM));
-        telemetry.update();
-        
-        ////ZERO OUT ARM EXTENDER////
-        //if (digitalTouch.getState() == false) {
-          //arm_ELEVATOR_angle-=400 * (now_time-last_time);
-        //}
+We're adding Java 8 editor support to this SDK release. 
+To enable support, you can enable `Enable beta Java 8 editor features` in the Settings menu.
 
-        
-        ////----WHEEL DRIVING----////
-        /*
-        whl_LB_percent = gamepad1.left_stick_y;
-        whl_LF_percent = gamepad1.left_stick_y;
-        whl_RB_percent = gamepad1.right_stick_y;
-        whl_RF_percent = gamepad1.right_stick_y;
-        */
-        float drv_stick_y = gamepad1.left_stick_y;
-        float drv_stick_x = gamepad1.left_stick_x;
+For an example, take the following code snippet from
+the `ConceptTelemetry`[0] sample you might have already seen in prior years
 
-        if (Math.abs(gamepad1.left_stick_y) > Math.abs(gamepad1.left_stick_x)) {
-          whl_LB_percent = drv_stick_y;
-          whl_LF_percent = drv_stick_y;
-          whl_RB_percent = drv_stick_y;
-          whl_RF_percent = drv_stick_y;
-        }
-        else {
-          if (drv_stick_x > 0) {
-            whl_RF_percent = drv_stick_x * 1;
-            whl_RB_percent = drv_stick_x * -1;
-            whl_LF_percent = drv_stick_x * -1;
-            whl_LB_percent = drv_stick_x * 1;
-          }
-          
-          if (drv_stick_x < 0) {
-            whl_LF_percent = drv_stick_x * -1;
-            whl_LB_percent = drv_stick_x * 1;
-            whl_RB_percent = drv_stick_x * -1;
-            whl_RF_percent = drv_stick_x * 1;
-          }
-        }
-        whl_corrections(); // Corrects/Adjusts power for correct results
-        
-        //Set power of motors to their corresponding variables
-        whl_LB.setPower(whl_LB_percent);
-        whl_RB.setPower(whl_RB_percent);
-        whl_LF.setPower(whl_LF_percent);
-        whl_RF.setPower(whl_RF_percent);
-        
-        //Set position of arm and claw motors to their corresponding variables.
-        
-        claw_GRIP.setPosition(claw_GRIP_angle);
-        wrist_ROT.setPosition(wrist_ROT_percent);
-        arm_ROT.setTargetPosition(-Help.degreesToTick(arm_ROT_angle));
-        arm_EXT.setTargetPosition(-Help.degreesToTick(arm_EXT_angle));
-        arm_ELEVATOR1.setTargetPosition(+Help.degreesToTick(arm_ELEVATOR_angle));
-        arm_ELEVATOR2.setTargetPosition(-Help.degreesToTick(arm_ELEVATOR_angle));
-
-        susan_ROT.setPosition(susan_ROT_percent);
-        
-        
-
-        
-        
-        telemetry.update();
-      }
+```
+/**
+ * As an illustration, the first line on our telemetry display will display the
+ * battery voltage. The idea here is that it's expensive to compute the voltage
+ * (at least for purposes of illustration) so you don't want to do it unless the
+ * data is <em>actually</em> going to make it to the driver station (recall that
+ * telemetry transmission is throttled to reduce bandwidth use.
+ * Note that getBatteryVoltage() below returns 'Infinity' if there's no voltage
+ * sensor attached.
+ *
+ * @see Telemetry#getMsTransmissionInterval()
+ */
+telemetry.addData("voltage", "%.1f volts", new Func<Double>() {
+    @Override public Double value() {
+        return getBatteryVoltage();
     }
-    
-    
-    
-  }
-  
-  public void gamepadInputHandling(double now_time) {
-    /*
-    if (gamepad1.left_bumper) {
-      everything_universalscale = 0.4;
-      wheel_universalscale = 0.3;
-    }
-    else {
-      everything_universalscale = 1;
-      wheel_universalscale = 0.8;
-    }
-    if (gamepad1.right_bumper) {
-      wheel_equalizerscale = 0.3;
-    }
-    else {
-      wheel_equalizerscale = 0;
-    }*/
+});
+```
 
-    //dpad left/right wrist rotation
-    if (gamepad1.dpad_left) {
-      wrist_ROT_percent = 0.8;
-      wrist_ROT_pos += (now_time-last_time);
-    }
-    else if (gamepad1.dpad_right) {
-      wrist_ROT_percent = 0.3;
-      wrist_ROT_pos -= (now_time-last_time);
-    }
-    else {
-      wrist_ROT_percent = 0.5;
-    }
+There's a few ways to rewrite the same snippet to be more concise by using 
+what's called a lambda (lamb-da) expression[1] from the new Java 8 editor support.
 
-    //dpad up/down claw open/close
-    if (gamepad1.dpad_up) {
-      claw_GRIP_angle += 0.5 * (now_time-last_time);
-    }
-    else if (gamepad1.dpad_down) {
-      claw_GRIP_angle -= 0.5 * (now_time-last_time);
-    }
+The part of the code that simply defines what's called an "anonymous class"
+can be actually rewritten as a lambda expression.
 
-    // Y A arm ROT up down
-    if (gamepad1.y) {
-      arm_ELEVATOR_angle-=400 * (now_time-last_time);
-    } 
-    else if (gamepad1.a) {
-      arm_ELEVATOR_angle+=400 * (now_time-last_time);
+The anonymous class above is the following section:
+```
+new Func<Double>() {
+    @Override public Double value() {
+        return getBatteryVoltage();
     }
-
-    // B X arm EXT forward back
-    if (gamepad1.b) {
-      arm_EXT_angle-=450 * (now_time-last_time);
-    } 
-    else if (gamepad1.x) {
-      arm_EXT_angle+=450 * (now_time-last_time);
-    }
-
-     if (gamepad1.left_bumper) {
-      susan_ROT_percent = 0.8;
-      susan_ROT_pos += (now_time-last_time);
-    }
-    else if (gamepad1.right_bumper) {
-      susan_ROT_percent = 0.3;
-      susan_ROT_pos -= (now_time-last_time);
-    }
-    else {
-      susan_ROT_percent = 0.5;
-    }
-    
-    if (gamepad1.back) {
-      arm_ROT_angle+=20 * (now_time-last_time);
-    }
-    else if (gamepad1.start) {
-      arm_ROT_angle-=20 * (now_time-last_time);
-    }
-
-    ////----BOUNDARIES----////
-
-
-    if (arm_EXT_angle < -123123123) { 
-      arm_EXT_angle = 0;
-      
-    }
-    else if (arm_EXT_angle > 123123123) {
-      arm_EXT_angle = 1300;
-    }
-
-    //Boundaries of the arm vertical rotation
-    
-    if (arm_ELEVATOR_angle > 2480) {
-      arm_ELEVATOR_angle = 2480;
-    }
-    else if (arm_ELEVATOR_angle < 0) {
-      arm_ELEVATOR_angle = 0;
-    }
-
-    // Boundaries of the claw
-    if (claw_GRIP_angle < 0.28) {
-      claw_GRIP_angle = 0.28;
-    }
-    else if (claw_GRIP_angle > 0.85) {
-      claw_GRIP_angle = 0.85;
-    }
-
-  }
-  public void whl_corrections() {
-      whl_RF_percent = (float) (whl_RF_percent * -0.5);
-      whl_RB_percent = (float) (whl_RB_percent * -0.6 );
-      whl_LF_percent = (float) (whl_LF_percent * -0.5);
-      whl_LB_percent = (float) (whl_LB_percent * -0.6);
-  }
-  
-  
 }
+```
 
-class Help {
-  public static int degreesToTick (int degrees) {
-      int tickDegreeRatio = 5;
+What you actually care about is the return value (`getBatteryVoltage()`)
+of the function you defined, not the wrapping syntax (`new Func<Double>{...}`).
 
-      return degrees/tickDegreeRatio;
-  }
-  public static int degreesToTick (float degrees) {
-      int tickDegreeRatio = 5;
+As a general pattern, for a function that takes no arguments, you can remove
+the class declaration, method return type, and method name and the compiler
+will get what you are implying.
 
-      return (int) degrees/tickDegreeRatio;
-  }
-  
-  public static double numSign (double num) {
-    if (num >= 0) {
-      return 1.01;
-    }
-    else {
-      return -1.01;
-    }
-  }
+To see this visually, this is the rewritten lambda expression:
+```
+() -> {
+    // this area wrapped by the curly braces is the lambda body
+    return getBatteryVoltage();
 }
+```
+
+This leaves the lambda body intact and is a useful pattern if your return value
+needs a multiline calculation.
+
+In this case, we only have the method return taking a single line, so we can
+"unwrap" the return expression:
+```
+() -> getBatteryVoltage()
+```
+
+This again leaves a bit of room to make more concise, applying the same pattern,
+what can we remove?
+
+```
+this::getBatteryVoltage
+```
+
+This is called a method reference, which requires only two things, the object and
+the method you want to call on the object,
+Advanced note: the `this` comes from the `this` that was implied indirectly in the anonymous class
+usage through a capture, but now you actually need to add this `this` reference.
+
+Put all together, the same snippet can be rewritten simply as:
+```
+telemetry.addData("voltage", "%.1f volts", this::getBatteryVoltage);
+```
+
+
+We're still bound by some limitations of the Android platform, you can check
+https://developer.android.com/studio/write/java8-support-table for known 
+limitations for more advanced features.
+
+### Known limitations
+- Android 6.0 Marshmallow devices cannot use Java 8 features
+- `IntStream` usage via direct usage can cause Robot Controller crashes. 
+  - You can use `Arrays.of(new int[]{1})` as a workaround.
+
+0: https://github.com/FIRST-Tech-Challenge/FtcRobotController/blob/00cbf344526d991ce33452437018af9f119d22d1/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptTelemetry.java#L110-L123
+1: https://docs.oracle.com/javase/tutorial/java/javaOO/lambdaexpressions.html
+2: https://docs.oracle.com/javase/tutorial/java/javaOO/anonymousclasses.html#accessing
+
+
+
+
