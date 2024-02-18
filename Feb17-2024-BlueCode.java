c66839fd47ad7615b5bd9bdc2be95649fdc8a724
ImNotAnAmateurKV <101678251+ImNotAnAmateurKV@users.noreply.github.com>
2024-02-17 16:15:43 -0800

    e

--- Feb17-2024-BlueCode.java
+++ Feb17-2024-BlueCode.java
@@ -30,9 +30,6 @@ import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 import org.firstinspires.ftc.robotcore.external.navigation.Temperature;
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
@@ -247,56 +244,58 @@ public class Blue2024 extends LinearOpMode {
                 autoDriveHandling(0.5,-0.5,0.5,-0.5);
             }
             telemetryTfod();
-            //sleep(20);
-        }
-        //Based on bias, decide on path taken
-        //this is a bad way to choose
-        //setWheelMode("power");
-        if (LeftObjectDetected > CenterObjectDetected && LeftObjectDetected > RightObjectDetected) {
-            //Set Angle
-            if (!autoFirstAction) {
-                desiredRobotAngle = Help.angleCorrection(imu.getAngularOrientation().firstAngle, -45);
-                autoFirstAction = true;
-            }
-            if (!a2)
-                rotate("", desiredRobotAngle);
-            if (!a2 && runtime.seconds() - code_start_time < 7) {
-                a2 = true;
-                setWheelMode("position");
-                autoDriveHandling(0.5,0.5,0.5,0.5);
-            } 
-        }
-        else if (CenterObjectDetected > LeftObjectDetected && CenterObjectDetected > RightObjectDetected) {
-            //Go Middle
-            //Set Angle
-            if (!autoFirstAction) {
-                desiredRobotAngle = Help.angleCorrection(imu.getAngularOrientation().firstAngle, 0);
-                autoFirstAction = true;
-            }
-            if (!a2)
-                rotate("", desiredRobotAngle);
-            if (!a2 && runtime.seconds() - code_start_time < 7) {
-                a2 = true;
-                setWheelMode("position");
-                autoDriveHandling(0.5,0.5,0.5,0.5);
-            } 
         }
         else {
-            //Go Right
-            //Set Angle
-            if (!autoFirstAction) {
-                desiredRobotAngle = Help.angleCorrection(imu.getAngularOrientation().firstAngle, 45);
-                autoFirstAction = true;
-            }
-            if (!a2)
-                rotate("", desiredRobotAngle);
-            if (!a2 && runtime.seconds() - code_start_time < 7) {
-                a2 = true;
-                setWheelMode("position");
-                autoDriveHandling(0.5,0.5,0.5,0.5);
-            } 
+          visionPortal.stopStreaming();
+          visionPortal.close();
+          if (LeftObjectDetected > CenterObjectDetected && LeftObjectDetected > RightObjectDetected) {
+              //Set Angle
+              if (!autoFirstAction) {
+                  desiredRobotAngle = Help.angleCorrection(imu.getAngularOrientation().firstAngle, -45);
+                  autoFirstAction = true;
+                  setWheelMode("power");
+              }
+              if (!a2)
+                  rotate("", desiredRobotAngle);
+              if (!a2 && runtime.seconds() - code_start_time < 7) {
+                  a2 = true;
+                  setWheelMode("position");
+                  autoDriveHandling(0.5,0.5,0.5,0.5);
+              } 
+          }
+          else if (CenterObjectDetected > LeftObjectDetected && CenterObjectDetected > RightObjectDetected) {
+              //Go Middle
+              //Set Angle
+              if (!autoFirstAction) {
+                  desiredRobotAngle = Help.angleCorrection(imu.getAngularOrientation().firstAngle, 0);
+                  autoFirstAction = true;
+                  setWheelMode("power");
+              }
+              if (!a2)
+                  rotate("", desiredRobotAngle);
+              if (!a2 && runtime.seconds() - code_start_time < 7) {
+                  a2 = true;
+                  setWheelMode("position");
+                  autoDriveHandling(0.5,0.5,0.5,0.5);
+              } 
+          }
+          else {
+              //Go Right
+              //Set Angle
+              if (!autoFirstAction) {
+                  desiredRobotAngle = Help.angleCorrection(imu.getAngularOrientation().firstAngle, 45);
+                  autoFirstAction = true;
+                  setWheelMode("power");
+              }
+              if (!a2)
+                  rotate("", desiredRobotAngle);
+              if (!a2 && runtime.seconds() - code_start_time < 7) {
+                  a2 = true;
+                  setWheelMode("position");
+                  autoDriveHandling(0.5,0.5,0.5,0.5);
+              } 
+          }
         }
-
 
         if (rightangle_active) {
           if (Math.abs(Help.trueAngleDif(desiredRobotAngle,imu.getAngularOrientation().firstAngle)) > 5){
@@ -376,132 +375,7 @@ public class Blue2024 extends LinearOpMode {
     //telemetry.update();
   }
   
-  public void gamepadInputHandling(double now_time) {
-    if (gamepad1.x) {
-      start_down = true;
-    }
-    else {
-      if (start_down) {
-        alignRobot = !alignRobot;
-        aprilTagInfos = null;
-      }
-      start_down = false;
-    }
-    //Rotate 90
-    // If there are any joystick moveemnts during this, cancel rotation
-    if (gamepad1.dpad_right && !rightangle_active) {
-      rightangle_active = true;
-      desiredRobotAngle = Help.angleCorrection(imu.getAngularOrientation().firstAngle, -90);
-
-    }
-    else if (gamepad1.dpad_left && !rightangle_active){
-      rightangle_active = true;
-      desiredRobotAngle = Help.angleCorrection(imu.getAngularOrientation().firstAngle, 90);
-    }
-    
-
-
-    if (gamepad2.y) {
-      servo_ROTATER_power = 0.3;
-    }
-    else if (gamepad2.a) {
-      servo_ROTATER_power =- 0.3;
-    }
-    if (!gamepad2.a&&!gamepad2.y) {
-      servo_ROTATER_power = 0;
-    }
-    
-    if (gamepad2.dpad_left) {
-      servo_CLAW_power += -5 * (now_time-last_time);
   
-    }
-    else if  (gamepad2.dpad_right) {
-      servo_CLAW_power += 5 * (now_time-last_time);
-  
-    }
-    
-     if (gamepad1.dpad_left) {
-      //arm_HOOKUP_speed = 5;
-  
-    }
-    else if  (gamepad1.dpad_right) {
-      //arm_HOOKDOWN_speed = 5;
-  
-    }
-    
-    if (gamepad1.dpad_left) {
-      //arm_HOOKDOWN_speed =- 5;
-  
-    }
-    else if  (gamepad1.dpad_right) {
-      //arm_HOOKUP_speed =- 5;
-  
-    }
-    
-    else {
-    servo_CLAW_power = 0;
-      
-    }
-    if (gamepad1.y) {
-      servo_DRONE_power = 20;
-    }
-    else if (gamepad1.a) {
-      servo_DRONE_power =- 10;
-      
-    }
-    
-    //if (gamepad1.y) {
-      //servo_DRONE2_power = 20;
-    //}
-    //else if (gamepad1.a) {
-      //servo_DRONE2_power =- 10;
-      
-    //}
-    
-    if (!gamepad2.left_bumper) {
-      left_bumper_DOWN = false;
-    }
-
-    if (gamepad2.dpad_up) {
-      //arm_ELEVATOR_speed+= 100 * (now_time-last_time);
-    }
-    else if (gamepad2.dpad_down){
-      //arm_ELEVATOR_speed-= 100* (now_time-last_time);
-    }
-
-    if (gamepad2.right_bumper && claw_ELEVATOR_position <470) {
-      if (!gamepad1.b) {
-         claw_ELEVATOR_position+= 150 * (now_time-last_time);
-      }
-      else{
-        claw_ELEVATOR_position = 470;
-      }
-    }
-    else if (gamepad2.right_trigger > 0.2 && claw_ELEVATOR_position >-35) {
-      if (!gamepad1.b) {
-        claw_ELEVATOR_position-= 150 * (now_time-last_time);
-      }
-      else {
-        claw_ELEVATOR_position=-35;
-      }
-    }
-  }
-  
-  public void clock(double now_time) {
-    if (gamepad2.start) {
-      clock_active = false;
-      clock_timer = clock_timer_MAX;
-    }
-    else if (!clock_active && !gamepad1.atRest()) {
-      clock_active = true;
-    }
-    
-    if (clock_active) {
-      clock_timer -= (now_time-last_time);
-      if (clock_timer < 0.0)
-        clock_timer = 0.0;
-    }
-  }
   
   public void whl_corrections() {
     //1st mult: individual wheel balance
@@ -591,118 +465,7 @@ public class Blue2024 extends LinearOpMode {
   
   //Ready
   
-  public void twoDriveHandling(double Y, double X) {
-    whl_LB_percent = 0;
-    whl_LF_percent = 0;
-    whl_RB_percent = 0;
-    whl_RF_percent = 0;
-    
-    whl_LB_percent += Y;
-    whl_LF_percent += Y;
-    whl_RB_percent += Y;
-    whl_RF_percent += Y;
-    
-    whl_LB_percent += X;
-    whl_LF_percent += X;
-    whl_RB_percent -= X;
-    whl_RF_percent -= X;
-    
-    whl_LB_percent = whl_LB_percent/1.5;
-    whl_LF_percent = whl_LF_percent/1.5;
-    whl_RB_percent = whl_RB_percent/1.5;
-    whl_RF_percent = whl_RF_percent/1.5;
-  }
   
-  public void tankDriveHandling() {
-
-    if (Math.abs(gamepad1.left_stick_y) > 0.2 || Math.abs(gamepad1.right_stick_y) > 0.2){
-      joystick_active = true;
-    }
-    else {
-      joystick_active = false;
-    }
-
-    boolean dif = Math.abs((gamepad1.left_stick_y+gamepad1.left_stick_x))>Math.abs((gamepad1.right_stick_x+gamepad1.right_stick_y));
-    
-    float drv_stick_y2 = gamepad1.right_stick_y;
-    float drv_stick_x2 = gamepad1.right_stick_x;
-    float truth = (Math.abs(gamepad1.right_stick_y) - Math.abs(gamepad1.left_stick_y) > 0) ? gamepad1.right_stick_y : gamepad1.left_stick_y;
-  
-    
-
-    /*
-    if (gamepad1.dpad_right) {
-      whl_RF_percent = 2;
-      whl_RB_percent = -1.5f;
-      whl_LF_percent = -2;
-      whl_LB_percent = 1.5f;
-    }
-    
-    else if (gamepad1.dpad_left) {
-      whl_LF_percent = 2;
-      whl_LB_percent = -1.5f;
-      whl_RB_percent = 1.5f;
-      whl_RF_percent = -2;
-    }*/
-    
-    if (gamepad1.left_stick_y > 0.9 && gamepad1.right_stick_y < -0.9) {
-      whl_RF_percent += -1;
-      whl_RB_percent += -1f;
-      whl_LF_percent += 1;
-      whl_LB_percent += 1f;
-    }
-    
-    else if (gamepad1.left_stick_y < -0.9 && gamepad1.right_stick_y > 0.9) {
-      whl_LF_percent += -1;
-      whl_LB_percent += -1f;
-      whl_RB_percent += 1f;
-      whl_RF_percent += 1;
-    }
-    else {
-      whl_LB_percent += gamepad1.left_stick_y;
-      whl_LF_percent += gamepad1.left_stick_y;
-      whl_RB_percent += gamepad1.right_stick_y;
-      whl_RF_percent += gamepad1.right_stick_y;
-    }
-
-    if (gamepad1.right_bumper) {
-      whl_LB_percent += 0.5;
-      whl_LF_percent += 0.5;
-      whl_RB_percent += 0.5;
-      whl_RF_percent += 0.5;
-    }
-    else if (gamepad1.left_bumper) {
-      whl_LB_percent -= 0.5;
-      whl_LF_percent -= 0.5;
-      whl_RB_percent -= 0.5;
-      whl_RF_percent -= 0.5;
-    }
-
-    if (gamepad1.left_trigger > 0.8) {
-      whl_LB_percent += 1;
-      whl_LF_percent -= 0.9;
-      whl_RB_percent -= 1;
-      whl_RF_percent += 0.9;
-      if (!isStrafing){
-        isStrafing = true;
-        strafeStartingAngle = imu.getAngularOrientation().firstAngle;
-      }
-    }
-    else if (gamepad1.right_trigger > 0.8) {
-      whl_LB_percent -= 1;
-      whl_LF_percent += 0.9;
-      whl_RB_percent += 1;
-      whl_RF_percent -= 0.9;
-      if (!isStrafing){
-        isStrafing = true;
-        strafeStartingAngle = imu.getAngularOrientation().firstAngle;
-      }
-    }
-    else if (isStrafing) {
-      isStrafing = false;
-      strafeEndTime = runtime.seconds();
-    }
-   }
    
    private void initAprilTag() {
 
