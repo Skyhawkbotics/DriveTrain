1bb18c40e972b8f5abd0725fcb31d605b1073e10
Insignia101 <98855763+Insignia101@users.noreply.github.com>
2024-10-03 17:10:55 -0700

    Update helpModule.java

--- helpModule.java
+++ helpModule.java
@@ -57,37 +57,83 @@ public class Help {
     // Case 2: -2, 2, true dif is 4, actual dif 4
     // Case 1: 179, 1, true dif is 178, actual dif 178
 
-//                      __    
-// __ __    ________ __|  | __
-//|  |  \  /  ___/  |  \  |/ /
-//|  |  /  \___ \|  |  /    < 
-//|____/  /____  >____/|__|_ \
-//             \/           \/
-    
     double actualDif = Math.abs(imuAngle - desiredAngle);
     double polarity = Math.signum(imuAngle - desiredAngle);
-
+    
+    //If either imu or desired angle is negative, reverse then calibrate to basic system
+    imuAngle+=180;
+    desiredAngle+=180;
+    /*
+    if (imuAngle < 180) { 
+      imuAngle = 180 - imuAngle;
+    }
+    if (desiredAngle < 180) { 
+      desiredAngle = 180 - desiredAngle;
+    }
+    */
+    /*
+    if (imuAngle < 0) { 
+      imuAngle += 180;
+    }
+    if (desiredAngle < 0) {
+      desiredAngle += 180;
+    }
+    */
+    // possible range of values; 0 to 360
+    // Check from middle range
+    double polarity1;
+    double polarity2;
+    double distance1 = Math.abs(imuAngle-desiredAngle);
+    polarity1 = Math.signum(imuAngle-desiredAngle);
+    double distance2;
+    if (imuAngle > desiredAngle) {
+      distance2 = (360 - imuAngle) + desiredAngle;
+      polarity2 = -Math.signum(imuAngle-desiredAngle);
+    }
+    else {
+      distance2 = (360 - desiredAngle) + imuAngle;
+      polarity2 = -Math.signum(imuAngle-desiredAngle);
+    }
+    
+    //Check from outer bands
+    
     //double actualDif = Math.abs(imu.getAngularOrientation().firstAngle - desiredAngle);
     //double polarity = Math.signum(imu.getAngularOrientation().firstAngle - desiredAngle);
-    
+    double chosenDistance = (distance1<distance2) ? distance1 : distance2;
+    double chosenPolarity = (distance1<distance2) ? polarity1 : polarity2;
+    return (chosenDistance * chosenPolarity);
+    /*
     if (actualDif < 180) {
       return actualDif * polarity;
     }
     else {
       return (360.0 - actualDif) * polarity;
+    }*/
+  }
+  
+  public static double angleCorrection(double current, double increment) {
+    double output = current + increment;
+    if (output > 180) {
+      double overflow = output - 180;
+      output = -180 + overflow;
     }
+    else if (output < -180) {
+      double overflow = output + 180;
+      output = 180 + overflow;
+    }
+    return output;
   }
 
   public static double getAverage_aprilTagInfos(double[][] aprilTagInfos, int index) {
 
     int output = 0;
-    int MAX_ITERATIONS = index;
+    int MAX_ITERATIONS = 1;
 
     for (int i = 0; i < MAX_ITERATIONS; i++) {
-      output += aprilTagInfos[i][5];
+      output += aprilTagInfos[i][index];
     }
 
-    output = output / index;
+    output = output / MAX_ITERATIONS;
     return output;
   }
 }
